491ead67eee5add5fc96e8b28158cd0ebfc60b37
Phil <p.clauzel90@gmail.com>
2015-07-17 14:17:53 +0100

    Add routes for services

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
  
-   root 'static_pages#home'
+  root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
+  get 'tshirt-printing' => 'static_pages#tshirt'
+  get 'workwear' => 'static_pages#workwear'
+  get 'sublimation-printing' => 'static_pages#sublimation'
+  get 'display-printing' => 'static_pages#display'
+  get 'charity-vests' => 'static_pages#charity'
  
 end
